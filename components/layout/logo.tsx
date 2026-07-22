6e8070c174813e9bacb4cbbaed5021cea207be2e
manish1914 <manish@gfuturetech.com>
2026-07-22 13:38:32 +0530

    Logo fixed

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -32,18 +32,20 @@ export function Logo({
           className={inverse ? 'fill-white' : 'fill-navy-900 dark:fill-white'}
         />
 
-        <path
-          d="M9.2 23.8 16.8 7l2.7 6.05-3.32 7.3H12.7L11.1 23.8H9.2Z"
-          fill="#00E05C"
-          className="origin-center transition-transform duration-700 group-hover:scale-110"
-        />
+        <g transform="translate(17 17) scale(1.6) translate(-17 -17)">
+  <path
+    d="M9.2 23.8 16.8 7l2.7 6.05-3.32 7.3H12.7L11.1 23.8H9.2Z"
+    fill="#00E05C"
+    className="origin-center transition-transform duration-700 group-hover:scale-110"
+  />
 
-        <path
-          d="M20.1 14.35 25 25h-3.62l-1.42-3.2h-5.24l1.42-3.2h2.4l-1.2-2.67 2.76-1.58Z"
-          className={
-            inverse ? 'fill-navy-900' : 'fill-white dark:fill-navy-950'
-          }
-        />
+  <path
+    d="M20.1 14.35 24.3 23.5h-3.62l-1.42-3.2h-5.24l1.42-3.2h2.4l-1.2-2.67 2.76-1.58Z"
+    className={
+      inverse ? 'fill-navy-900' : 'fill-white dark:fill-navy-950'
+    }
+  />
+</g>
       </svg>
 
       <span className="flex min-w-0 flex-col justify-center">
